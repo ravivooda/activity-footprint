a1d6051f7ae61f08facf321526a9759ce3f938d8
Ravi Vooda <r.vooda@gmail.com>
2020-06-28 16:53:42 -0700

    Scaffolding Activity Footprint XCode project

---
+++ Activity Footprint/Activity Footprint/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Activity Footprint
+//
+//  Created by Ravi Vooda on 6/28/20.
+//  Copyright © 2020 Ravi Vooda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
